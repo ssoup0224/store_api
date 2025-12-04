0365a48ff7c6ec5d372ab41718d70da8465bb42b
정성국 <ssoup0224@gmail.com>
2025-12-04 18:17:19 +0900

    added cart and cart_item entities

---
+++ src/main/java/com/store_api/entities/CartItem.java
@@ -0,0 +1,29 @@
+package com.store_api.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ColumnDefault("1")
+    @Column(name = "quantity")
+    private Integer quantity;
+}
