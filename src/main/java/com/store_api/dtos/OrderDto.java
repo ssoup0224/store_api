f66a1fbe7155bb3a875d3cb33bf303ae7bbbbbcf
정성국 <ssoup0224@gmail.com>
2025-12-10 16:42:07 +0900

    added order dto

---
+++ src/main/java/com/store_api/dtos/OrderDto.java
@@ -0,0 +1,23 @@
+package com.store_api.dtos;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+public class OrderDto {
+
+    @Getter
+    @Setter
+    public static class CheckOutRequest {
+        @NotNull(message = "Cart ID is required.")
+        private UUID cartId;
+    }
+
+    @Getter
+    @Setter
+    public static class CheckOutResponse {
+        private Long orderId;
+    }
+}
